c63f6882b0e163a2ef50c0b894cd5c9fe466a5f7
Parsha Pourkhomami <parshap+git@gmail.com>
2013-07-30 08:12:40 +0000

    Implement functionality to pass test

---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var COMMAND = "fpcalc";
+
+var run = require("comandante"),
+	split = require("split");
+
+module.exports = function(path, options, callback) {
+	if ( ! callback) {
+		callback = options;
+		options = {};
+	}
+
+	var args = [];
+
+	if (options.length) {
+		args.push("-length", options.length);
+	}
+
+	args.push(path);
+
+	// Create a result object to build as data comes in
+	var result = {};
+
+	function onData(data) {
+		var parts = data.split("=", 2),
+			name = parts[0].toLowerCase(),
+			value = parts[1];
+
+		if (name) {
+			result[name.toLowerCase()] = value;
+		}
+	}
+
+	function onEnd() {
+		callback(null, result);
+	}
+
+	run(COMMAND, args)
+		.pipe(split())
+		.on("data", onData)
+		.on("end", onEnd)
+		.on("error", callback);
+};
