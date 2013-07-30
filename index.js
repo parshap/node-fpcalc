a10144e0d2d616f654a051871b64717fe68ba41f
Parsha Pourkhomami <parshap+git@gmail.com>
2013-07-30 21:22:23 +0000

    Use streams to parse output and handle errors

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
+/* jshint node:true */
 "use strict";
-
-var COMMAND = "fpcalc",
-	run = require("comandante"),
-	fpcalc = run.bind(null, COMMAND),
-	split = require("split");
 
 module.exports = function(file, options, callback) {
 	// Handle `options` parameter being optional
@@ -12,11 +8,8 @@ module.exports = function(file, options, callback) {
 		options = {};
 	}
 
-	var
-		// Result object to build as data is returned by fpcalc
-		result = {},
-		// Command-line arguments to pass to fpcalc
-		args = [];
+	// Command-line arguments to pass to fpcalc
+	var args = [];
 
 	// `-length` command-line argument
 	if (options.length) {
@@ -25,25 +18,93 @@ module.exports = function(file, options, callback) {
 
 	args.push(file);
 
-	function onData(data) {
-		// Data is given as lines like `FILE=path/to/file`, so we split the
-		// parts out and build the `results` object
-		var parts = data.split("=", 2),
-			name = parts[0].toLowerCase(),
-			value = parts[1];
-
-		if (name) {
-			result[name.toLowerCase()] = value;
-		}
-	}
-
-	fpcalc(args)
-		// parse one complete line at a time
-		.pipe(split())
-		// parse data into result object
-		.on("data", onData)
-		// return result object
-		.on("end", callback.bind(null, null, result))
-		// return error
-		.on("error", callback);
+	run(args)
+		.on("error", callback)
+		.pipe(parse(callback.bind(null, null)));
 };
+
+// -- Run fpcalc command
+
+var spawn = require("child_process").spawn,
+	fpcalc = spawn.bind(null, "fpcalc"),
+	es = require("event-stream"),
+	concat = require("concat-stream");
+
+// Runs the fpcalc tool and returns a readable stream that will emit stdout
+// or an error event if an error occurs
+function run(args) {
+	var
+		// Create fpcalc child process
+		cp = fpcalc(args),
+		// Create a through stream that passes through data but does not end
+		// when the source stream ends. We will manually end the stream once
+		// the child processes closes.
+		stream = es.through(null, function() {});
+
+	// Pass fpcalc stdout through the stream
+	cp.stdout.pipe(stream);
+
+	// Catch fpcalc stderr errors even when exit code is 0
+	// See https://bitbucket.org/acoustid/chromaprint/issue/2/fpcalc-return-non-zero-exit-code-if
+	cp.stderr.pipe(concat(function(data) {
+		data = data.toString();
+		if (data && data.slice(0, 6) === "ERROR:") {
+			stream.emit("error", new Error(data));
+		}
+	}));
+
+	// End the stream when the child processes closes
+	cp.on("close", function() {
+		stream.queue(null);
+	});
+
+	return stream;
+}
+
+// -- fpcalc stdout stream parsing
+
+function parse(callback) {
+	return es.pipeline(
+		// Parse one complete line at a time
+		es.split(),
+		// Only use non-empty lines
+		filter(Boolean),
+		// Parse each line into name/value pair
+		es.mapSync(parseData),
+		// Reduce data into single result object to pass to callback
+		reduce(function(result, data) {
+			result[data.name] = data.value;
+			return result;
+		}, {}, callback)
+	);
+}
+
+// Create a through stream that only passes data that passes the given test
+// @TODO Extract this to own module
+function filter(test) {
+	return es.mapSync(function(data) {
+		if (test(data)) {
+			return data;
+		}
+	});
+}
+
+// Reduce stream data into a single value
+// @TODO Extract this to own module
+function reduce(fn, acc, callback) {
+	return es.through(function(data) {
+		acc = fn(acc, data);
+	}, function() {
+		callback(acc);
+	});
+}
+
+// Data is given as lines like `FILE=path/to/file`, so we split the
+// parts out to a name/value pair
+function parseData(data) {
+	var parts = data.split("=", 2);
+	return {
+		name: parts[0].toLowerCase(),
+		value: parts[1],
+	};
+}
