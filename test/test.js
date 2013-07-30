f01a70e7de3af5c1d555d57102002572b8585ad1
Parsha Pourkhomami <parshap+git@gmail.com>
2013-07-30 21:16:11 +0000

    Add test for bad file path

--- test/test.js
+++ test/test.js
@@ -6,12 +6,19 @@ var path = require("path"),
 
 var TEST_FILE = path.join(__dirname, "test.mp3");
 
-test("test audio fingerprint", function(t) {
-	t.plan(4);
+test("get audio fingerprint", function(t) {
 	fpcalc(TEST_FILE, function(err, result) {
 		t.ifError(err);
 		t.ok(result.file);
 		t.ok(result.duration);
 		t.ok(result.fingerprint);
+		t.end();
+	});
+});
+
+test("bad file path", function(t) {
+	fpcalc("bad/path", function(err, result) {
+		t.ok(err);
+		t.end();
 	});
 });
